77e1958360fceb17c08922ca6cfda6688e865f8d
daho-jung <48465453+daho-jung@users.noreply.github.com>
2021-04-08 17:54:44 +0900

    Add files via upload

--- demo/src/main/java/com/example/demo/test/controller/TestController.java
+++ demo/src/main/java/com/example/demo/test/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.example.demo.test.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class TestController {
@@ -16,6 +20,21 @@ public class TestController {
 	public String getTestValue() {
 		String testvalue = "test String";
 		return testvalue;
+	}
+	
+	//jsp
+	@RequestMapping("/test")
+	public ModelAndView test() throws Exception{
+		ModelAndView mav = new ModelAndView("test");
+		mav.addObject("name", "haha");
+		
+		List<String> testList = new ArrayList<String>();
+		testList.add("a");
+		testList.add("b");
+		testList.add("c");
+		mav.addObject("list", testList);
+		
+		return mav;
 	}
 }
 
